88a95d569d8e3e3eef347008e79c266cf12810a5
cijic <altcode@ya.ru>
2015-07-02 23:16:39 +0300

    config: * Changed base URL;

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://onetimefile.local';
+$config['base_url'] = 'http://ci3.onetimefile.local';
 
 /*
 |--------------------------------------------------------------------------
